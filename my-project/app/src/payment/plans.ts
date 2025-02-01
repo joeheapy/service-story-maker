2732ff7d8f547a9021758856534f71da87bad869
joeheapy <joe.heapy@me.com>
2025-02-01 23:49:27 +0000

    Changed prices on product page

--- my-project/app/src/payment/plans.ts
+++ my-project/app/src/payment/plans.ts
@@ -1,54 +1,65 @@
-import { requireNodeEnvVar } from '../server/utils';
+import { requireNodeEnvVar } from '../server/utils'
 
-export type SubscriptionStatus = 'past_due' | 'cancel_at_period_end' | 'active' | 'deleted';
+export type SubscriptionStatus =
+  | 'past_due'
+  | 'cancel_at_period_end'
+  | 'active'
+  | 'deleted'
 
 export enum PaymentPlanId {
-  Hobby = 'hobby',
-  Pro = 'pro',
   Credits10 = 'credits10',
+  Credits50 = 'credits50',
+  Credits500 = 'credits500',
 }
 
 export interface PaymentPlan {
-  // Returns the id under which this payment plan is identified on your payment processor. 
+  // Returns the id under which this payment plan is identified on your payment processor.
   // E.g. this might be price id on Stripe, or variant id on LemonSqueezy.
-  getPaymentProcessorPlanId: () => string;
-  effect: PaymentPlanEffect;
+  getPaymentProcessorPlanId: () => string
+  effect: PaymentPlanEffect
 }
 
-export type PaymentPlanEffect = { kind: 'subscription' } | { kind: 'credits'; amount: number };
+export type PaymentPlanEffect =
+  | { kind: 'subscription' }
+  | { kind: 'credits'; amount: number }
 
 export const paymentPlans: Record<PaymentPlanId, PaymentPlan> = {
-  [PaymentPlanId.Hobby]: {
-    getPaymentProcessorPlanId: () => requireNodeEnvVar('PAYMENTS_HOBBY_SUBSCRIPTION_PLAN_ID'),
-    effect: { kind: 'subscription' },
-  },
-  [PaymentPlanId.Pro]: {
-    getPaymentProcessorPlanId: () => requireNodeEnvVar('PAYMENTS_PRO_SUBSCRIPTION_PLAN_ID'),
-    effect: { kind: 'subscription' },
-  },
   [PaymentPlanId.Credits10]: {
-    getPaymentProcessorPlanId: () => requireNodeEnvVar('PAYMENTS_CREDITS_10_PLAN_ID'),
+    getPaymentProcessorPlanId: () =>
+      requireNodeEnvVar('PAYMENTS_CREDITS_10_PLAN_ID'),
     effect: { kind: 'credits', amount: 10 },
   },
-};
+  [PaymentPlanId.Credits50]: {
+    getPaymentProcessorPlanId: () =>
+      requireNodeEnvVar('PAYMENTS_CREDITS_50_PLAN_ID'),
+    effect: { kind: 'credits', amount: 50 },
+  },
+  [PaymentPlanId.Credits500]: {
+    getPaymentProcessorPlanId: () =>
+      requireNodeEnvVar('PAYMENTS_CREDITS_500_PLAN_ID'),
+    effect: { kind: 'credits', amount: 500 },
+  },
+}
 
 export function prettyPaymentPlanName(planId: PaymentPlanId): string {
   const planToName: Record<PaymentPlanId, string> = {
-    [PaymentPlanId.Hobby]: 'Hobby',
-    [PaymentPlanId.Pro]: 'Pro',
     [PaymentPlanId.Credits10]: '10 Credits',
-  };
-  return planToName[planId];
+    [PaymentPlanId.Credits50]: '50 Credits',
+    [PaymentPlanId.Credits500]: '500 Credits',
+  }
+  return planToName[planId]
 }
 
 export function parsePaymentPlanId(planId: string): PaymentPlanId {
   if ((Object.values(PaymentPlanId) as string[]).includes(planId)) {
-    return planId as PaymentPlanId;
+    return planId as PaymentPlanId
   } else {
-    throw new Error(`Invalid PaymentPlanId: ${planId}`);
+    throw new Error(`Invalid PaymentPlanId: ${planId}`)
   }
 }
 
 export function getSubscriptionPaymentPlanIds(): PaymentPlanId[] {
-  return Object.values(PaymentPlanId).filter((planId) => paymentPlans[planId].effect.kind === 'subscription');
+  return Object.values(PaymentPlanId).filter(
+    (planId) => paymentPlans[planId].effect.kind === 'subscription'
+  )
 }
